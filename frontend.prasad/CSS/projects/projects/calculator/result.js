85c3fb36b221e0d8ba7479209016afcc25584d38
pandharemahesh685 <pandharemahesh685@gmail.com>
2025-02-02 17:56:00 +0530

    ks

---
+++ frontend.prasad/CSS/projects/projects/calculator/result.js
@@ -0,0 +1,24 @@
+function Solve(val)
+{
+    var v = document.getElementById('ans');
+    v.value += val;
+}
+
+function Result()
+{
+    var v1 = document.getElementById('ans').value;
+    var v2 = eval(v1);
+    document.getElementById('ans').value = v2;
+}
+
+function Clear()
+{
+    var input = document.getElementById('ans');
+    input.value= '';
+}
+
+function Back()
+{
+    var Back = document.getElementById('ans');
+    Back.value = Back.value.slice(0,-1);
+}
